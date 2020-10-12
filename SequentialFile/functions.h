627f0ef5ad808d1095855c68339092448ffd5a17
CarlosReategui <carlos.reategui@utec.edu.pe>
2020-10-12 00:42:15 -0500

    test shortcut

--- SequentialFile/functions.h
+++ SequentialFile/functions.h
@@ -8,13 +8,6 @@
 #include "Record.h"
 #include "SequentialFile.h"
 
-unsigned long getFileSize(std::string newFileName) {
-    std::ifstream file(newFileName, std::ios::ate | std::ios::binary);
-    unsigned long size = file.tellg();
-    file.close();
-    return size;
-}
-
 void shortPrintTeams(std::vector<Team<long> > teams, unsigned long totalOrderedRegisters) {
     std::cout << std::setw(4) << "ID" << " | " << std::setw(20) << "Equipo" << " | " << std::setw(4) << "next" << " | " 
         << std::setw(4) << "prev" << std::endl;
@@ -25,12 +18,5 @@ void shortPrintTeams(std::vector<Team<long> > teams, unsigned long totalOrderedR
         teams[i].shortPrint();
     }
 }
-
-// void searchAndPrint(SequentialFile<Team<long> > &sf, std::vector<Team<long> > records) {
-//     for (Team<long> record : records) {
-//         Team<long> foundRecord = sf.searchInOrderedRegisters(record.ID);
-//         foundRecord.shortPrint();
-//     }
-// }
 
 #endif
